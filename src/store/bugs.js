b534def45eb93f2d2563f265f2da8404e3145fd5
Hassaan Zuberi <mhazuberi@gmail.com>
2022-02-05 16:21:05 +0300

    implemented createslice of redux toolkit. this approach squashed everything types actions and reducers way less boilerplate

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,28 +1,29 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
-
-export const bugAdded = createAction("bugAdded");
-export const bugRemoved = createAction("bugRemoved");
-export const bugResolved = createAction("bugResolved");
+import { createSlice } from "@reduxjs/toolkit";
 
 let lastId = 0;
 
-const reducer = createReducer([], (reducer) => {
-  reducer
-    .addCase(bugAdded, (bugs, action) => {
+const bugs = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
       bugs.push({
         id: ++lastId,
         description: action.payload.description,
         resolved: false,
       });
-    })
-    .addCase(bugRemoved, (bugs, action) => {
+    },
+    bugRemoved: (bugs, action) => {
       const index = bugs.findIndex((bug) => bug.id === action.payload.id);
       bugs.splice(index, 1);
-    })
-    .addCase(bugResolved, (bugs, action) => {
+    },
+    bugResolved: (bugs, action) => {
       const index = bugs.findIndex((bug) => bug.id === action.payload.id);
       bugs[index].resolved = true;
-    });
+    },
+  },
 });
 
-export default reducer;
+export const { bugAdded, bugRemoved, bugResolved } = bugs.actions;
+
+export default bugs.reducer;
