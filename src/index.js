4805754fa8f4934043122978adf982ed634628f6
Hassaan Zuberi <mhazuberi@gmail.com>
2022-02-05 08:37:54 +0300

    created actions with redux toolkit. squashed action types and actions in one liners

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ const unsubscribe = store.subscribe(() => {
   console.log("Store Changed!", store.getState());
 });
 
-store.dispatch(bugAdded("Bug 01"));
-store.dispatch(bugAdded("Bug 02"));
-store.dispatch(bugAdded("Bug 03"));
-store.dispatch(bugResolved(1));
-store.dispatch(bugResolved(2));
+store.dispatch(bugAdded({ description: "Bug 01" }));
+store.dispatch(bugAdded({ description: "Bug 02" }));
+store.dispatch(bugAdded({ description: "Bug 03" }));
+store.dispatch(bugResolved({ id: 1 }));
+store.dispatch(bugResolved({ id: 2 }));
 
 // unsubscribe();
 
-store.dispatch(bugRemoved(1));
+store.dispatch(bugRemoved({ id: 1 }));
 
 console.log(store.getState());
