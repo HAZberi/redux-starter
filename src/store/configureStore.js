25affd75f06bd02a1d15f9ed2de375ea6395a009
Hassaan Zuberi <mhazuberi@gmail.com>
2022-02-06 09:07:54 +0300

    parameterized redux middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,5 +4,8 @@ import reducer from "./reducer";
 import logger from "./middleware/logger";
 
 export default function () {
-  return configureStore({ reducer, middleware: [logger] });
+  return configureStore({
+    reducer,
+    middleware: [logger("Logging in Production")],
+  });
 }
--- src/store/middleware/logger.js
+++ src/store/middleware/logger.js
@@ -1,4 +1,5 @@
-const logger = store => next => action => {
+const logger = param => store => next => action => {
+    console.log("Passed params to middleware", param);
     console.log("Store", store);
     console.log("Next", next);
     console.log("Action Dispatched", action);
